5fff103f357586c2cbadc37ca1fc09bde0bd876b
RomuloGFreitas <romulo.freitas.rg@gmail.com>
2024-10-31 23:10:31 -0300

    Criacao do projeto

---
+++ CarRental/Entities/CarRental.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Aluguel.Entities
+{
+    public class CarRental
+    {
+        public DateTime DateStart { get; set; }
+        public DateTime DateFinish { get; set; }
+        public Vehicle Vehicle { get; set; }
+        public Invoice Invoice { get; set; }
+
+        public CarRental(DateTime dateStart, DateTime dateFinish, Vehicle vehicle)
+        {
+            DateStart = dateStart;
+            DateFinish = dateFinish;
+            Vehicle = vehicle;
+            Invoice = null;
+        }
+    }
+} 
---
+++ CarRental/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using Aluguel.Entities;
+using Aluguel.Services;
+
+namespace Aluguel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ente com os dados do Aluguel");
+            Console.WriteLine("Car model: ");
+            string model = Console.ReadLine();
+            Console.WriteLine("Data de inicio:");
+            DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+            Console.WriteLine("Data de retorno:");
+            DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter price per hour: ");
+            double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("Enter price per day: ");
+            double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            CarRental carRental = new CarRental(start, finish, new Vehicle(model));
+
+            RentalService rentalService = new RentalService(hour, day);
+
+            rentalService.ProcessInvoice(carRental);
+
+            Console.WriteLine("INVOICE: ");
+            Console.WriteLine(carRental.Invoice);
+        }
+    }
+}
