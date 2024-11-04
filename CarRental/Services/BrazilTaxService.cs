5dc973f99183784801f695ee7a85d9732feeead6
RomuloGFreitas <romulo.freitas.rg@gmail.com>
2024-11-04 20:23:09 -0300

    Adição de serviço de Interface

--- CarRental/Services/BrazilTaxService.cs
+++ CarRental/Services/BrazilTaxService.cs
@@ -1,6 +1,6 @@
 ﻿namespace Aluguel.Services
 {
-    public class BrazilTaxService
+    public class BrazilTaxService : ITaxService
     {
         public double Tax(double amount)
         {
--- CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
+++ CarRental/obj/Debug/net6.0/CarRental.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fff103f357586c2cbadc37ca1fc09bde0bd876b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CarRental")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
